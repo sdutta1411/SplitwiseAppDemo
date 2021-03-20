64b951efc93d078ae5d2e94afb3d5fa67692d640
Shubham <shubhamdutta1411@gmail.com>
2021-03-20 02:13:12 -0700

    Implemented Connection Pooling

--- Backend/config.js
+++ Backend/config.js
@@ -1,19 +1,44 @@
 const mysql = require("mysql");
 
-var mysqlConnection = mysql.createConnection({
+// Without Connection Pooling
+//
+// var mysqlConnection = mysql.createConnection({
+//     host: "database-1.cxoqk6j22ojk.us-east-2.rds.amazonaws.com",
+//     user: "admin",
+//     password: "12345678",
+//     database: "splitwise",
+//     multipleStatements: true
+// });
+
+// mysqlConnection.connect((err) => {
+//     if (!err) {
+//         console.log("Connected");
+//     } else {
+//         console.log("Connection Failed");
+//     }
+// });
+
+//module.exports = mysqlConnection;
+
+//------------------------------------------------------------------------------------------
+
+
+// With Connection Pooling
+
+const pool = mysql.createPool({
     host: "database-1.cxoqk6j22ojk.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "12345678",
     database: "splitwise",
-    multipleStatements: true
 });
 
-mysqlConnection.connect((err) => {
+pool.getConnection((err, connection) => {
     if (!err) {
+        console.log('connected as id ' + connection.threadId);
         console.log("Connected");
     } else {
         console.log("Connection Failed");
     }
 });
 
-module.exports = mysqlConnection;
+module.exports = pool;
--- Backend/controllers/fetchExpenses-controller.js
+++ Backend/controllers/fetchExpenses-controller.js
@@ -5,25 +5,41 @@ module.exports.fetchExpenses = function (req, res) {
     let groupName = req.body.groupName;
     console.log(groupName);
 
-    connection.query('SELECT * FROM expenses WHERE groupName = ? order by creationDate desc', [groupName], function (error, results, fields) {
-        if (error) {
-            res.json({
-                status: false,
-                message: 'There are some error with query'
-            })
-        } else {
-            if (results.length > 0) {
+    if (!groupName) {
+        connection.query('SELECT * FROM expenses order by creationDate desc', [groupName], function (error, results, fields) {
+            if (error) {
+                res.json({
+                    status: false,
+                    message: 'There are some error with query'
+                })
+            } else {
                 res.json({
                     status: true,
                     data: results
                 })
             }
-            else {
+        })
+    } else {
+        connection.query('SELECT * FROM expenses WHERE groupName = ? order by creationDate desc', [groupName], function (error, results, fields) {
+            if (error) {
                 res.json({
                     status: false,
-                    message: "No Expenses for the Group"
-                });
+                    message: 'There are some error with query'
+                })
+            } else {
+                if (results.length > 0) {
+                    res.json({
+                        status: true,
+                        data: results
+                    })
+                }
+                else {
+                    res.json({
+                        status: false,
+                        message: "No Expenses for the Group"
+                    });
+                }
             }
-        }
-    });
+        });
+    }
 }
